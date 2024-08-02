aa8717855aa552b2fd28dbad1188ffff9a5367a4
Greg Neustroev <G.Neustroev@tudelft.nl>
2024-08-02 12:26:19 +0200

    Add the samplers to `__init__.py`

--- stochastic_rsa/sampling/__init__.py
+++ stochastic_rsa/sampling/__init__.py
@@ -1,5 +1,6 @@
 """Provides samplers for choosing points in the space [0, Inf) x X"""
 from .sampler import Sampler
+from .sobol import SobolSampler
+from .grid import GridSampler
 
-
-__all__ = ["Sampler"]
+__all__ = ["Sampler", "SobolSampler", "GridSampler"]
