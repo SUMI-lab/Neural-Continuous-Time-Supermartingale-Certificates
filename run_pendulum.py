cdca21874b01a0dc5b1f6e817aeb5017a5a76720
Greg Neustroev <G.Neustroev@tudelft.nl>
2024-08-04 15:14:06 +0200

    Improve plots

--- run_pendulum.py
+++ run_pendulum.py
@@ -1,6 +1,8 @@
 import torch
 import numpy.random as npr
 import matplotlib.pyplot as plt
+from matplotlib import colors
+from matplotlib.patches import Rectangle
 import controlled_sde
 from rl_agent import TanhPolicy
 import stochastic_rsa as rsa
@@ -95,7 +97,7 @@ target_set = rsa.membership_sets.SublevelSet(
     1.0
 )
 unsafe_set = rsa.membership_sets.MembershipSet(
-    lambda x: torch.abs(x[:, 0]) >= 7.0
+    lambda x: x[:, 0] >= 5.0
 )
 reach_avoid_probability, stay_probability = 0.9, 0.9
 
@@ -118,12 +120,55 @@ x0 = torch.tensor([STARTING_SPEED, STARTING_ANGLE],
 ts = torch.linspace(0, DURATION, T_SIZE, device=device)
 
 sample_paths = sde.sample(x0, ts, method="srk").squeeze()
-plot_data = sample_paths.cpu().numpy()
 
-fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-ax.set_theta_offset(torch.pi/2)
-fig, ax = plt.subplots()
-ax.plot(plot_data[:, :, 0], plot_data[:, :, 1])
+# Plot
+fig, (ax1, ax2) = plt.subplots(1, 2)
+
+with torch.no_grad():
+    x = torch.tensor(sampler.sample_space(16384),
+                     dtype=torch.float32,
+                     device=device
+                     )
+    c = ax1.tricontourf(x[:, 0].squeeze().numpy(),
+                        x[:, 1].squeeze().numpy(),
+                        certificate.net(x).clamp(min=1e-20).squeeze().numpy(),
+                        norm=colors.LogNorm())
+
+fig.colorbar(c, ax=ax1)
+
+ax1.set_xlim([-MAX_SPEED, MAX_SPEED])
+ax1.set_ylim([-MAX_ANGLE, MAX_ANGLE])
+ax1.add_patch(Rectangle((-0.5, 0.875 * torch.pi), 1, 0.25 * torch.pi,
+                        edgecolor='yellow',
+                        facecolor='none',
+                        lw=2))
+ax1.add_patch(Rectangle((-1, -torch.pi/4), 2, torch.pi/2,
+                        edgecolor='green',
+                        facecolor='none',
+                        lw=2))
+ax1.add_patch(Rectangle((5, -3.5 * torch.pi), 4, 7 * torch.pi,
+                        edgecolor='red',
+                        facecolor='none',
+                        lw=2))
+
+path_data = sample_paths.cpu().numpy()
+ax2.plot(path_data[:, :, 0], path_data[:, :, 1])
+
+ax2.set_xlim([-MAX_SPEED, MAX_SPEED])
+ax2.set_ylim([-MAX_ANGLE, MAX_ANGLE])
+ax2.add_patch(Rectangle((-0.5, 0.875 * torch.pi), 1, 0.25 * torch.pi,
+                        edgecolor='yellow',
+                        facecolor='none',
+                        lw=2))
+ax2.add_patch(Rectangle((-1, -torch.pi/4), 2, torch.pi/2,
+                        edgecolor='green',
+                        facecolor='none',
+                        lw=2))
+ax2.add_patch(Rectangle((5, -3.5 * torch.pi), 4, 7 * torch.pi,
+                        edgecolor='red',
+                        facecolor='none',
+                        lw=2))
+
 plt.show()
 
 sde.render(sample_paths, ts)
