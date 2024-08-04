ee076e72493c91ca679f20241afe0f4f6a65664c
Greg Neustroev <G.Neustroev@tudelft.nl>
2024-08-04 14:29:50 +0200

    Add device to policy

--- rl_agent/policy.py
+++ rl_agent/policy.py
@@ -1,5 +1,5 @@
 """Provides the policy used in the experiments."""
-
+import torch
 from torch import nn
 
 
@@ -8,11 +8,11 @@ class TanhPolicy(nn.Sequential):
     A policy with three layers and tanh activations.
     """
 
-    def __init__(self, n_out: int = 1, n_hidden: int = 64):
+    def __init__(self, n_out: int = 1, n_hidden: int = 64, device: torch.device | str = "cpu"):
         super().__init__(
-            nn.LazyLinear(n_hidden),
+            nn.LazyLinear(n_hidden, dtype=torch.float32, device=device),
             nn.Tanh(),
-            nn.LazyLinear(n_hidden),
+            nn.LazyLinear(n_hidden, dtype=torch.float32, device=device),
             nn.Tanh(),
-            nn.LazyLinear(n_out),
+            nn.LazyLinear(n_out, dtype=torch.float32, device=device),
         )
