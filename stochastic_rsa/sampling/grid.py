ba7d665f8accb769b78c5d99f98720b22d75d423
Greg Neustroev <G.Neustroev@tudelft.nl>
2024-08-05 10:29:50 +0200

    Fix a wrong function name

--- stochastic_rsa/sampling/grid.py
+++ stochastic_rsa/sampling/grid.py
@@ -11,7 +11,7 @@ class GridSampler(Sampler):
 
     def sample_from_unit_box(self, n: int = 100) -> np.ndarray:
         n_dim = self.n_dim
-        n_points_per_dim = int(np.ceil(np.pow(n, 1.0 / n_dim)))
+        n_points_per_dim = int(np.ceil(np.power(n, 1.0 / n_dim)))
         points_along_one_dimension = np.linspace(0, 1, n_points_per_dim)
         grid = np.meshgrid(*((points_along_one_dimension,) * n_dim))
         grid = np.concatenate(grid).reshape(
